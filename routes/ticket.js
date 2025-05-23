57bcec6e68c6b8070f81ce9ab7991dd59f734bfc
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2025-05-23 09:29:41 +0530

    pushing server code to git

---
+++ routes/ticket.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { bookTicket, getUserTickets } from "../controllers/ticket.js";
+import { verifyToken } from "../middlewares/verify.js";
+
+const router = express.Router();
+
+router.post("/book", bookTicket);
+router.get("/my-ticket",verifyToken, getUserTickets);
+
+export default router;
